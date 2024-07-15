cce4db50acfda3f5642563ddee528b6f70bf9db8
Harry <isaqueduarte17@gmail.com>
2024-07-14 21:17:53 -0300

    feat: load env variables and add error handler (middleware)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import itemRoutes from "./routes/items.routes";
+import itemRoutes from "./modules/item/item.routes";
+import { loadEnvVariables } from "./config/environment";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+
+loadEnvVariables();
 
 const app = express();
 
@@ -7,5 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/items", itemRoutes);
+
+// Middleware - Error handler
+app.use(errorHandler);
 
 export default app;
